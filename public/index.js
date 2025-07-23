3a7b23e4ac58eef540adeab32e187427cb2fcbe4
Flame787 <mbrezovic77@gmail.com>
2025-07-23 15:45:40 +0200

    hide navbar when scrolling down

--- public/index.js
+++ public/index.js
@@ -23,6 +23,11 @@
     let prevScrollPos = window.scrollY;
     const navbar = document.getElementById("navigation");
 
+    // get navbar height (it varies depending on resolution):
+    function getNavbarHeight() {
+      return navbar.offsetHeight;
+    }
+
     window.onscroll = function () {
       let currentScrollPos = window.scrollY;
 
@@ -31,7 +36,7 @@
         navbar.style.top = "0";
       } else {
         // If scrolling down, hide navbar:
-        navbar.style.top = "-100px"; // Navbar height - adjustable
+        navbar.style.top = `-${getNavbarHeight()}px`; // Navbar height in pixel - adjustable
       }
 
       prevScrollPos = currentScrollPos;
@@ -46,7 +51,7 @@
     navbar.addEventListener("mouseleave", () => {
       let currentScrollPos = window.scrollY;
       if (prevScrollPos < currentScrollPos) {
-        navbar.style.top = "-100px";
+        navbar.style.top = `-${getNavbarHeight()}px`;
       }
     });
 
